45e2127ec71ada6a30e852695f949db9e74bc8e3
Ludwig Simonsson <93864945+LudwigSimonsson@users.noreply.github.com>
2023-05-04 11:00:37 +0200

    Create 12.cpp

---
+++ 12.cpp
@@ -0,0 +1,5 @@
+int a = 10;
+int b = 20;
+int c = a + b;
+//I det optimerade exemplet används istället int-variabler för att hålla värdena, vilket eliminerar behovet av typomvandling och förbättrar prestandan.
+
