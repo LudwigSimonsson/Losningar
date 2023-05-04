da5144924170f3490496ddbe4346077e09d2ce45
Ludwig Simonsson <93864945+LudwigSimonsson@users.noreply.github.com>
2023-05-04 10:56:24 +0200

    Create 6.cpp

---
+++ 6.cpp
@@ -0,0 +1,7 @@
+void function(int x) {
+  if (x < 0) {
+    MyClass a; //Genom att lägga klassen inuti vår if-sats så har vi format behovsaktivering
+    a.setError(true);
+    // annan kod här
+  }
+}
